dcc9eefdbfa5f8a8d0cd489e1f5be97c75aa49bf
Michiel van der Meer <michielmeer@live.nl>
2015-11-25 13:17:15 +0100

    synced movements!

--- SimultaneousNaoControl/main.py
+++ SimultaneousNaoControl/main.py
@@ -2,10 +2,29 @@ import argparse
 import sys
 import time
 import math
+import cwiid
 from threading import Thread
 
 from naomanager import NaoManager, DEFAULT_PORT
 from naoqi import ALProxy
+
+def connectWiimote(wm):
+    i = 0
+    print 'Press 1+2 on your Wiimote now...'
+    while not wm:
+        try:
+            wm = cwiid.Wiimote()
+            print("Connected")
+        except RuntimeError:
+            if(i>5):
+                print("Cannot connect to Wiimote")
+                quit()
+            print("Failed attempt",i)
+            i += 1
+
+    wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+    wm.led = 1
+    return wm
 
 # Main program to perform the measurements
 def main(args):
@@ -19,45 +38,95 @@ def main(args):
             ip = nao
             port = DEFAULT_PORT
             naos.addnao(ip, int(port))
-            
+    wm = None
+    wm = connectWiimote(wm)
+    x  = 0.0
+    y  = 0.0
+    theta  = 0.0
+    frequency  = 0.3
+    CommandFreq = 0.5
+    print("Ready for moving")
 
+    print("Connected Wiimote")
     starttime = time.time()
 
-    for nao in naos:
-       nao.behavior.post.runBehavior('standup')
-   
-    time.sleep(15.0)
+    while True:
+        buttons = wm.state['buttons']
+        if (buttons & cwiid.BTN_HOME):
+            print("Closing Connection")
+            for nao in naos:
+                wm.rumble = 1
+                time.sleep(1)
+                wm.rumble = 0
+                nao.motion.killAll()
+                nao.stop()
+                wm = None
+                exit()
+        elif (buttons & cwiid.BTN_UP):
+            print("Move forward")
+            for nao in naos:
+                x = 0.5
+                nao.motion.setWalkTargetVelocity(x, y, theta, frequency)
+        elif (buttons & cwiid.BTN_DOWN):
+            print("Move backward")
+            for nao in naos:
+                x  = -0.5
+                nao.motion.setWalkTargetVelocity(x, y, theta, frequency)
+        elif (buttons & cwiid.BTN_LEFT):
+            print("Walking left")
+            for nao in naos:
+                y = 0.5
+                nao.motion.setWalkTargetVelocity(x, y, theta, frequency)
+        elif (buttons & cwiid.BTN_RIGHT):
+            print("Walking right")
+            for nao in naos:
+                y = -0.5
+                nao.motion.setWalkTargetVelocity(x, y, theta, frequency)
+        elif (buttons & cwiid.BTN_MINUS):
+            print("Going to rest")
+            for nao in naos:
+                nao.motion.rest()
+        elif (buttons & cwiid.BTN_PLUS):
+            print("Standup")
+            for nao in naos:
+                nao.posture.goToPosture("StandInit", 0.5)
+        else:
+            print("Doing nothing..")
+            for nao in naos:
+                x = 0
+                y = 0
+                nao.motion.setWalkTargetVelocity(x, y, theta, frequency)
+            time.sleep(CommandFreq) # sleep after every command
+            nao.behavior.post.runBehavior('standup')
 
     # Do a simply interpolation
     #target1 = math.radians(119.5)
     #target2 = math.radians(29.5)
     #naos.motion.post.angleInterpolation(['HeadYaw', 'HeadPitch'],
-    #        [[target1, -target1, 0.0], [target2, -target2, 0.0]], 
+    #        [[target1, -target1, 0.0], [target2, -target2, 0.0]],
     #        [[1.0, 5.0, 7.0], [1.0, 5.0, 7.0]], True)
     #naos.behavior.post.runBehavior('kecup')
     #naos.behavior.post.runBehavior('vangelis')
-    
+
     # for nao in naos:
     #     nao.behavior.post.runBehavior('vangelis')
-    
-    
+
+
     # Wait
     #while time.time() < (starttime+10.0):
     #    time.sleep(0.1)
-            
+
     # Stop logging if needed
     #naos.stop()
-    
+
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--nao', action='append')
     args = parser.parse_args()
-    
+
     if not args.nao:
         print('No naos to test specified!')
         sys.exit(0)
-        
+
     main(args)
-    
-    
--- wii_walk.py
+++ wii_walk.py
@@ -7,117 +7,117 @@ import time
 from naoqi import ALProxy
 import cwiid
 
+class wiiController(self):
+    def StiffnessOn(proxy):
+        # We use the "Body" name to signify the collection of all joints
+        pNames = "Body"
+        pStiffnessLists = 1.0
+        pTimeLists = 1.0
+        proxy.stiffnessInterpolation(pNames, pStiffnessLists, pTimeLists)
 
-def StiffnessOn(proxy):
-    # We use the "Body" name to signify the collection of all joints
-    pNames = "Body"
-    pStiffnessLists = 1.0
-    pTimeLists = 1.0
-    proxy.stiffnessInterpolation(pNames, pStiffnessLists, pTimeLists)
+    def StiffnessOff(proxy):
+        pNames = "Body"
+        pStiffnessLists = 0.0
+        pTimeLists = 1.0
+        proxy.stiffnessInterpolation(pNames, pStiffnessLists, pTimeLists)
 
-def StiffnessOff(proxy):
-    pNames = "Body"
-    pStiffnessLists = 0.0
-    pTimeLists = 1.0
-    proxy.stiffnessInterpolation(pNames, pStiffnessLists, pTimeLists)
+    def connectWiimote(wm):
+        i = 0
+        print 'Press 1+2 on your Wiimote now...'
+        while not wm:
+            try:
+                wm = cwiid.Wiimote()
+                print("Connected")
+            except RuntimeError:
+                if(i>5):
+                    print("Cannot connect to Wiimote")
+                    quit()
+                print("Failed attempt",i)
+                i += 1
 
-def connectWiimote(wm):
-    i = 0
-    print 'Press 1+2 on your Wiimote now...'
-    while not wm:
+        wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+        wm.led = 1
+        return wm
+
+
+    def main(robotIP):
+        # Init proxies.
         try:
-            wm = cwiid.Wiimote()
-            print("Connected")
-        except RuntimeError:
-            if(i>5):
-                print("Cannot connect to Wiimote")
-                quit()
-            print("Failed attempt",i)
-            i += 1
+            motionProxy = ALProxy("ALMotion", robotIP, 9559)
+        except Exception, e:
+            print "Could not create proxy to ALMotion"
+            print "Error was: ", e
 
-    wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
-    wm.led = 1
-    return wm
+        try:
+            postureProxy = ALProxy("ALRobotPosture", robotIP, 9559)
+        except Exception, e:
+            print "Could not create proxy to ALRobotPosture"
+            print "Error was: ", e
 
+        # Set NAO in Stiffness On
+        StiffnessOn(motionProxy)
 
-def main(robotIP):
-    # Init proxies.
-    try:
-        motionProxy = ALProxy("ALMotion", robotIP, 9559)
-    except Exception, e:
-        print "Could not create proxy to ALMotion"
-        print "Error was: ", e
+        # Send NAO to Pose Init
+        postureProxy.goToPosture("StandInit", 0.5)
 
-    try:
-        postureProxy = ALProxy("ALRobotPosture", robotIP, 9559)
-    except Exception, e:
-        print "Could not create proxy to ALRobotPosture"
-        print "Error was: ", e
+        #####################
+        ## Enable arms control by Walk algorithm
+        #####################
+        motionProxy.setWalkArmsEnabled(True, True)
+        #~ motionProxy.setWalkArmsEnabled(False, False)
 
-    # Set NAO in Stiffness On
-    StiffnessOn(motionProxy)
+        #####################
+        ## FOOT CONTACT PROTECTION
+        #####################
+        #~ motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", False]])
+        motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
 
-    # Send NAO to Pose Init
-    postureProxy.goToPosture("StandInit", 0.5)
+        # Movement! ------
+        x  = 0.0
+        y  = 0.0
+        theta  = 0.0
+        frequency  = 0.3
+        CommandFreq = 0.5
+        print("Ready for moving")
+        while True:
+            buttons = wm.state['buttons']
 
-    #####################
-    ## Enable arms control by Walk algorithm
-    #####################
-    motionProxy.setWalkArmsEnabled(True, True)
-    #~ motionProxy.setWalkArmsEnabled(False, False)
-
-    #####################
-    ## FOOT CONTACT PROTECTION
-    #####################
-    #~ motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", False]])
-    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
-
-    # Movement! ------
-    x  = 0.0
-    y  = 0.0
-    theta  = 0.0
-    frequency  = 0.3
-    CommandFreq = 0.5
-    print("Ready for moving")
-    while True:
-        buttons = wm.state['buttons']
-
-        if (buttons & cwiid.BTN_HOME):
-            print("Closing Connection")
-            wm.rumble = 1
-            time.sleep(1)
-            wm.rumble = 0
-            motionProxy.killAll()
-            motionProxy.rest()
-            exit()
-        elif (buttons & cwiid.BTN_UP):
-            print("Move forward")
-            x = 0.5
-            motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
-        elif (buttons & cwiid.BTN_DOWN):
-            print("Move backward")
-            x  = -0.5
-            motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
-        elif (buttons & cwiid.BTN_LEFT):
-            print("Walking left")
-            y = 0.5
-            motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
-        elif (buttons & cwiid.BTN_RIGHT):
-            print("Walking right")
-            y = -0.5
-            motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
-        elif (buttons & cwiid.BTN_MINUS):
-            print("Going to rest")
-            motionProxy.rest()
-        elif (buttons & cwiid.BTN_PLUS):
-            print("Standup")
-            postureProxy.goToPosture("StandInit", 0.5)
-        else:
-            print("Doing nothing..")
-            x = 0
-            y = 0
-            motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
-        time.sleep(CommandFreq) # sleep after every command
+            if (buttons & cwiid.BTN_HOME):
+                print("Closing Connection")
+                wm.rumble = 1
+                time.sleep(1)
+                wm.rumble = 0
+                motionProxy.killAll()
+                motionProxy.rest()
+                exit()
+            elif (buttons & cwiid.BTN_UP):
+                print("Move forward")
+                x = 0.5
+                motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
+            elif (buttons & cwiid.BTN_DOWN):
+                print("Move backward")
+                x  = -0.5
+                motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
+            elif (buttons & cwiid.BTN_LEFT):
+                print("Walking left")
+                y = 0.5
+                motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
+            elif (buttons & cwiid.BTN_RIGHT):
+                print("Walking right")
+                y = -0.5
+                motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
+            elif (buttons & cwiid.BTN_MINUS):
+                print("Going to rest")
+                motionProxy.rest()
+            elif (buttons & cwiid.BTN_PLUS):
+                print("Standup")
+                postureProxy.goToPosture("StandInit", 0.5)
+            else:
+                print("Doing nothing..")
+                x = 0
+                y = 0
+                motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
+            time.sleep(CommandFreq) # sleep after every command
 
 if __name__ == "__main__":
     robotIp = "192.168.1.49"
